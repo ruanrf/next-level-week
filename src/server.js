919ed7c87f125fb66a1b70e1db8ff195d31da184
Ruan <ruan_rf@hotmail.com>
2020-08-09 16:46:19 -0300

    increment backend and update pages

--- src/server.js
+++ src/server.js
@@ -1,37 +1,109 @@
+// data
 const vacancies = [
     {
         company: "Itaú Unibanco", 
         logo: "https://www.itau.com.br/content/dam/itau/varejo/logo-app-Itau.png", 
         contact: "ruan_rf@hotmail.com", 
         description: "Absque sudore et labore nullum opus perfectum est", 
-        "job-area": 1,
-        "job-position": 5,
-        "job-mode": 3,
+        area: 1,
+        position: 5,
+        mode: 3,
         salary: "4900",
+        // benefits: "xpto",
         region: [2],
-        country: ["Beta"],
-        city: ["お酒"]
+        country: ["Brazil"],
+        city: ["São Paulo"]
     }
 ]
 
+const jobArea = [
+    "Front-end",
+    "Back-end",
+    "Full-stack",
+    "QA and Testing",
+    "Mobile (Android)",
+    "Mobile (iOS)",
+    "Mobile (Hybrid)",
+    "Design",
+    "UX",
+    "Product Owner",
+    "Business",
+    "Others"
+]
+
+const jobPosition = [
+    "Intern",
+    "Junior",
+    "Middle",
+    "Senior",
+    "Trainee",
+    "Lead",
+    "CTO",
+    "Director",
+    "Others"
+]
+
+const jobRegion = [
+    "Europe",
+    "North America",
+    "South America",
+    "Oceania",
+    "Asia",
+    "Africa",
+    "N/A"
+]
+
+const jobMode = [
+    "Office-only",
+    "Remote-only",
+    "Hybrid"
+]
+
+// functions
+function getJobArea(areaNumber) {
+    return jobArea[areaNumber]
+}
+
 function pageLanding(req, res) {
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 }
 
 function pageJobs(req, res) {
-    return res.sendFile(__dirname + "/views/jobs.html")
+    const filters = req.query
+    return res.render("jobs.html", { vacancies, filters, jobArea, jobPosition, jobRegion, jobMode })    
 }
 
 function pageAddVacancy(req, res) {
-    return res.sendFile(__dirname + "/views/add-vacancy.html")
+    const data = req.query
+    const isNotEmpty = Object.keys(data).length > 0
+
+    if (isNotEmpty) { 
+        data.area = getJobArea(data.area)
+        vacancies.push(data)
+        return res.redirect("/jobs")
+    }
+    
+    return res.render("add-vacancy.html", { jobArea, jobPosition, jobMode, jobRegion })
 }
 
+// server
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
+// nunjucks configuration (template engine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
+// server start and execution
+server
+// static files
+.use(express.static("public")) 
+// routes
 .get('/', pageLanding)
 .get("/jobs", pageJobs)
 .get("/add-vacancy", pageAddVacancy)
+// start server
 .listen(5500)
