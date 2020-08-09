deb14495ae26c14ca64b1a6f7cf7874731ee8e69
Ruan <ruan_rf@hotmail.com>
2020-08-09 01:35:00 -0300

    implement local server and fix add-vacancy

--- src/server.js
+++ src/server.js
@@ -1,15 +1,37 @@
+const vacancies = [
+    {
+        company: "Itaú Unibanco", 
+        logo: "https://www.itau.com.br/content/dam/itau/varejo/logo-app-Itau.png", 
+        contact: "ruan_rf@hotmail.com", 
+        description: "Absque sudore et labore nullum opus perfectum est", 
+        "job-area": 1,
+        "job-position": 5,
+        "job-mode": 3,
+        salary: "4900",
+        region: [2],
+        country: ["Beta"],
+        city: ["お酒"]
+    }
+]
+
+function pageLanding(req, res) {
+    return res.sendFile(__dirname + "/views/index.html")
+}
+
+function pageJobs(req, res) {
+    return res.sendFile(__dirname + "/views/jobs.html")
+}
+
+function pageAddVacancy(req, res) {
+    return res.sendFile(__dirname + "/views/add-vacancy.html")
+}
+
 const express = require('express')
 const server = express()
 
 server.use(express.static("public"))
 
-.get('/', (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-})
-.get("/jobs", (req, res) => {
-    return res.sendFile(__dirname + "/views/jobs.html")
-})
-.get("/add-vacancy", (req, res) => {
-    return res.sendFile(__dirname + "/views/add-vacancy.html")
-})
+.get('/', pageLanding)
+.get("/jobs", pageJobs)
+.get("/add-vacancy", pageAddVacancy)
 .listen(5500)
