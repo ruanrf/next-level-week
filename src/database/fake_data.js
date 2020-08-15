d85923f955781dd1f402795944a203af488eb63b
Ruan <ruan_rf@hotmail.com>
2020-08-15 00:59:34 -0300

    refactor server.js and add fake data for testing

---
+++ src/database/fake_data.js
@@ -0,0 +1,17 @@
+// data
+const vacancies = [
+    {
+        company: "Itaú Unibanco", 
+        logo: "https://www.itau.com.br/content/dam/itau/varejo/logo-app-Itau.png", 
+        contact: "ruan_rf@hotmail.com", 
+        description: "Absque sudore et labore nullum opus perfectum est", 
+        area: 1,
+        position: 5,
+        mode: 3,
+        salary: "4900",
+        // benefits: "xpto",
+        region: [2],
+        country: ["Brazil"],
+        city: ["São Paulo"]
+    }
+]
